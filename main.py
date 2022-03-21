4d6bab7aed606d9efc0862fd87c0673b7e56e75c
James Pugado <jamespugado@yahoo.com>
2022-03-21 15:43:59 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,43 @@
+import mysql.connector
+from kivymd.app import MDApp
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.config import Config
+from kivy.core.window import Window
+
+Window.size = (320, 680)
+myDb = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='',
+    database='balayan_taxpayer'
+)
+
+myCursor = myDb.cursor()
+
+
+class WindowManager(ScreenManager):
+    pass
+
+
+class SecondPage(Screen):
+    pass
+
+
+class HomePage(Screen):
+    Config.set("graphics", "width", "320")
+    Config.set("graphics", "height", "680")
+    Config.set("graphics", "borderless", "0")
+    Config.set("graphics", "resizable", "0")
+
+# Designate Our .kv file
+kv = Builder.load_file('sample.kv')
+
+
+class SampleApp(MDApp):
+    def build(self):
+        return kv
+
+
+if __name__ == "__main__":
+    SampleApp().run()
